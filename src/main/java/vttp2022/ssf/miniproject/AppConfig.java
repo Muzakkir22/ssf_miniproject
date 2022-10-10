951c5c8311a8d6b6725091ecc0a47b619448de1c
Muzakkir22 <muzakkir22@gmail.com>
2022-10-10 09:04:28 +0800

    Added redis password

--- src/main/java/vttp2022/ssf/miniproject/AppConfig.java
+++ src/main/java/vttp2022/ssf/miniproject/AppConfig.java
@@ -1,5 +1,7 @@
 package vttp2022.ssf.miniproject;
 
+import java.beans.JavaBean;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
